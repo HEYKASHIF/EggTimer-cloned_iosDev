64042755d12345cd5210c10e2cbe475fbc5c4929
HEYKASHIF <kashifmohd269@gmail.com>
2023-07-26 01:14:03 +0530

    first commit

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,9 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
     let eggTimes = ["Soft": 5, "Medium": 7, "Hard": 12]
+//    var timer = 60
+//    func countdown() {
+//        timer -= 1
+//    }
+
+    
     
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
+    
+        
+
+//        var clock = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: Selector(("countdown")), userInfo: nil, repeats: true)
+
         print(sender.currentTitle!)
         
         let hardness = sender.currentTitle!
