03fdb997cbfa5e196334b40e75461c311c7540af
HEYKASHIF <kashifmohd269@gmail.com>
2023-07-26 06:33:25 +0530

    first commit11

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -12,9 +12,6 @@ import AVFoundation
 class ViewController: UIViewController
 {
     var player: AVAudioPlayer!
-    override func viewDidLoad() {
-            super.viewDidLoad()
-        }
     func playSound() {
             let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
             player = try! AVAudioPlayer(contentsOf: url!)
